eb089249331c34bd3d76ca99e01f4894f0bc4c4c
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2017-12-28 00:05:35 +0100

    removing commented code

--- src/FluentImposter.Core/IImposter.cs
+++ src/FluentImposter.Core/IImposter.cs
@@ -6,29 +6,4 @@ namespace FluentImposter.Core
     {
         Imposter Build(ImposterDefinition imposterDefinition);
     }
-
-    //public class TestImposter: IImposter
-    //{
-    //    public Response CreateResponse()
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-
-    //    public Imposter Build(ImposterDefinition imposterDefinition)
-    //    {
-    //        imposterDefinition.IsOfType(ImposterType.REST)
-    //                          .When(r => r.Body.Content.Contains(""))
-    //                          .Then(a => new TestResponseCreator()
-    //                                        .CreateResponse())
-    //                                        .When;
-    //    }
-    //}
-
-    //public class TestResponseCreator: IResponseCreator
-    //{
-    //    public Response CreateResponse()
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-    //}
 }

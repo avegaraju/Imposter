a068b246f545582c3b9a903fb3429edbf61278c7
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2018-01-16 22:14:43 +0100

    add aws sdk packages. check exixtance of tables in amazon dynamodb

--- src/FluentImposter.DataStore.AwsDynamoDb/AwsDynamoDbDataStore.cs
+++ src/FluentImposter.DataStore.AwsDynamoDb/AwsDynamoDbDataStore.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 using FluentImposter.Core;
 
@@ -6,14 +11,49 @@ namespace FluentImposter.DataStore.AwsDynamoDb
 {
     public class AwsDynamoDbDataStore: IDataStore
     {
+        private readonly AmazonDynamoDBClient _amazonDynamoDbClient;
+
+        public AwsDynamoDbDataStore()
+        {
+            _amazonDynamoDbClient = new AmazonDynamoDBClient();
+
+            CreateTables();
+        }
+
         public Guid CreateSession()
         {
-            throw new NotImplementedException();
+            var sessionId = Guid.NewGuid().ToString();
+
+            PutItemRequest putItemRequest = new PutItemRequest("FI_SESSION",
+                                                               new Dictionary<string, AttributeValue>()
+                                                               {
+                                                                   {
+                                                                       "SESSIONID",
+                                                                       new AttributeValue(sessionId)
+                                                                   }
+                                                               });
+            var putItemResponse  = _amazonDynamoDbClient.PutItemAsync(putItemRequest).Result;
+
+            return Guid.Parse(sessionId);
         }
 
         public void EndSession(Guid guid)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreateTables()
+        {
+            if (!TablesExists())
+            {
+                
+            }
+        }
+
+        private bool TablesExists()
+        {
+            return _amazonDynamoDbClient.ListTablesAsync(new ListTablesRequest())
+                                 .Result.TableNames.Any(t => t.Equals("FI_SESSION"));
         }
     }
 }

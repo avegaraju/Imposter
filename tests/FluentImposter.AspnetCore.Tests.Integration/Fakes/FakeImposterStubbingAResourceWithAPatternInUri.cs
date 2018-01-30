69560209c6d8f08a9056d2fe7ca98603eb2b25c4
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2018-01-30 22:59:24 +0100

    add test to prove that pattern in uri works.

---
+++ tests/FluentImposter.AspnetCore.Tests.Integration/Fakes/FakeImposterStubbingAResourceWithAPatternInUri.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+
+using FluentImposter.Core.Entities;
+
+namespace FluentImposter.AspnetCore.Tests.Integration.Fakes
+{
+    internal class FakeImposterStubbingAResourceWithAPatternInUri
+    {
+        private readonly HttpMethod _httpMethod;
+
+        public FakeImposterStubbingAResourceWithAPatternInUri(HttpMethod httpMethod)
+        {
+            _httpMethod = httpMethod;
+        }
+
+        public Imposter Build()
+        {
+            return new ImposterDefinition("test")
+                    .StubsResource("products/{productId}", _httpMethod)
+                    .When(r => r.Content.Contains("dummy"))
+                    .Then(new DummyResponseCreator())
+                    .Build();
+        }
+    }
+}
